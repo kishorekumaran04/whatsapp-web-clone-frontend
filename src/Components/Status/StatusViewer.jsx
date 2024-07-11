7331b312bccae7929a6829adc47523313e835191
Kishore Kumaran <kishore.kumaran04@gmail.com>
2024-07-11 17:36:12 -0400

    added navigation buttons to statusviewer page

--- src/Components/Status/StatusViewer.jsx
+++ src/Components/Status/StatusViewer.jsx
@@ -1,11 +1,16 @@
 import React, { useEffect, useState } from 'react';
 import { stories } from './DummyStories';
 import ProgressBar from './ProgressBar';
+import { IoIosArrowRoundBack } from 'react-icons/io';
+import { BsArrowLeft } from 'react-icons/bs';
+import { AiOutlineClose } from 'react-icons/ai';
+import { useNavigate } from 'react-router-dom';
 
 const StatusViewer = () => {
 
     const [currentStoryIndex, setCurrentStoryIndex] = useState(0);
     const [activeIndex, setActiveIndex] = useState(0);
+    const navigate = useNavigate();
 
     const handleNextStory = () => {
         if(currentStoryIndex < stories.length-1){
@@ -26,9 +31,13 @@ const StatusViewer = () => {
         return ()=>clearInterval(intervalId);
     },[currentStoryIndex])
 
+    const handleNavigate = () => {
+        navigate(-1);
+    }
+
     return (
         <div>
-            <div className='flex justify-center items-center h-[100vh] bg-slate-900'>
+            <div className='relative flex justify-center items-center h-[100vh] bg-slate-900'>
                 <div className='relative'>
                     <img 
                         className='max-h-[96vh] object-contain'
@@ -38,6 +47,17 @@ const StatusViewer = () => {
                     <div className='absolute top-0 flex w-full'> 
                         {stories.map((item, index)=><ProgressBar key={index} duration={2000} index={index} activeIndex={activeIndex}/>)}
                     </div>
+                </div>
+
+                <div>
+                    <BsArrowLeft
+                        onClick={handleNavigate} 
+                        className='text-white text-2xl cursor-pointer absolute top-5 left-10' 
+                    />
+                    <AiOutlineClose 
+                        onClick={handleNavigate}
+                        className='text-white text-2xl cursor-pointer absolute top-5 right-10' 
+                    />
                 </div>
             </div>
         </div>
