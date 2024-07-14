f73bef4a96b79b27d8e6a70f06dff91fc1dd72c6
Kishore Kumaran <kishore.kumaran04@gmail.com>
2024-07-14 00:34:12 -0400

    integrating redux

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import HomePage from "./Components/HomePage";
 import Status from "./Components/Status/Status";
 import StatusViewer from './Components/Status/StatusViewer';
+import Signin from './Components/Register/Signin';
+import Signup from './Components/Register/Signup';
 
 function App() {
 
@@ -12,6 +14,8 @@ function App() {
         <Route path="/" element={<HomePage/>}></Route>
         <Route path="/status" element={<Status/>}></Route>
         <Route path="/status/:userId" element={<StatusViewer/>}></Route>
+        <Route path="/signin" element={<Signin/>}></Route>
+        <Route path="/signup" element={<Signup/>}></Route>
       </Routes>
     </div>
   )
